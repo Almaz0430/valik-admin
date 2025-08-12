228be0ff1a229551605776b60baa041b8b20696c
Scissord <scissxrd@gmail.com>
2025-08-12 16:27:21 +0500

    fix

--- src/utils/axiosConfig.ts
+++ src/utils/axiosConfig.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const baseUrl = import.meta.env.VITE_NODE_ENV === 'development'
 ? 'http://localhost:8080'
-: 'http://localhost:8080';
+: 'https://api.valik.kz';
 
 export const api = axios.create({
   baseURL: baseUrl,
